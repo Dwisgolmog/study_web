529d62f177c827910e02551ff9b407166f9ef886
dnwn1211 <dnwn3027@gmail.com>
2023-03-25 23:57:17 +0900

    button_event_2
    button_2

--- 석준/Js/2__js/2_Dynamic_elements.js
+++ 석준/Js/2__js/2_Dynamic_elements.js
@@ -13,11 +13,24 @@ let before = document.getElementById('before');
 //이벤트
 let addBtn = document.getElementById('button');
 
+let target_remove = document.querySelector('#target-remove');
+
+let removebtn = document.querySelector('.remove_button');
+
 addBtn.addEventListener('click', function () {
   let li = document.createElement('li');
   let ul = document.querySelector('ul');
   li.textContent = inputBox.value; //텍스트 추가
+
+  let button = document.createElement('button');
+  button.textContent = 'x';
+  //button.setAttribute('class', 'remove_button');
+  button.classList.add('remove_button');
+  button.addEventListener('click', removeParentNode);
+  li.appendChild(button);
   ul.appendChild(li); //ul 안에 li 추가
+  inputBox.value = ''; // inputbox초기화
+  inputBox.focus(); //input자동 커서
 });
 
 before.addEventListener('click', function () {
@@ -30,3 +43,17 @@ before.addEventListener('click', function () {
   ul.insertBefore(li, targetLi);
   //alert('추가되었습니다.');
 });
+
+target_remove.addEventListener('click', function () {
+  let targetLi = document.querySelector('li#target');
+  targetLi.remove();
+});
+
+removebtn.addEventListener('click', removeParentNode);
+
+function removeParentNode(event) {
+  event.target.parentNode.remove();
+  inputBox.value = ''; // inputbox초기화
+  inputBox.focus(); //input자동 커서
+}
+d;
