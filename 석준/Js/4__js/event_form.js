ff2a93a10be42ff3d5532b8ee5df6a71ec43b8e0
dnwn1211 <dnwn3027@gmail.com>
2023-04-01 22:50:58 +0900

    event
    마우스, 키보드 이벤트

---
+++ 석준/Js/4__js/event_form.js
@@ -0,0 +1,23 @@
+let inputBox = document.querySelector('#inputbox');
+
+// inputBox.addEventListener('keypress', function (event) {
+//   console.log('keypress');
+// });
+// inputBox.addEventListener('keydown', function (event) {
+//   console.log('keydown');
+// });
+// inputBox.addEventListener('keyup', function (event) {
+//   console.log('event.key', event.key);
+//   console.log('event.key', event.keyCode);
+// });
+
+// inputBox.addEventListener('focus', function (event) {
+//   alert('focus');
+// });
+// inputBox.addEventListener('blur', function (event) {//focus 사라지면 발생
+//   alert('blur');
+// });
+// inputBox.addEventListener('change', function (event) {
+//   //value 변하면 발생
+//   console.log('change');
+// });
--- 석준/Js/4__js/event_mouse.js
+++ 석준/Js/4__js/event_mouse.js
@@ -1,16 +1,29 @@
-let rectangele=document.querySelector('#rectangle')
-rectangele.addEventListener('mousedown',function(event){
-    console.log('mousedown');
-})
-rectangele.addEventListener('mouseup',function(event){
-    console.log('mouseup');
-})
-rectangele.addEventListener('mouseenter',function(event){
-    console.log('mouseenter');
-})
-rectangele.addEventListener('mouseleave',function(event){
-    console.log('mouseleave');
-})
-rectangele.addEventListener('mousemove',function(event){
-    console.log('mousemove');
-})
+let rectangele = document.querySelector('#rectangle');
+let body = document.querySelector('body');
+
+rectangele.addEventListener('mousedown', function (event) {
+  // console.log('mousedown');
+  console.log('clientX: ', event.clientX, 'clientY : ', event.clientY); // 브라우저 기준
+  console.log('pageX: ', event.pageX, 'pageY : ', event.pageY); //전체 페이지 기준
+  console.log(event.target.getBoundingClientRect());
+});
+body.addEventListener('click', function () {
+  console.log('pagex ', event.pageX, 'pagey ', event.pageY);
+  let div = document.createElement('div');
+  div.classList.add('circle');
+  div.style.left = event.pageX - 25 + 'px';
+  div.style.top = event.pageY - 25 + 'px';
+  body.appendChild(div);
+});
+// rectangele.addEventListener('mouseup',function(event){
+//     console.log('mouseup');
+// })
+// rectangele.addEventListener('mouseenter',function(event){
+//     console.log('mouseenter');
+// })
+// rectangele.addEventListener('mouseleave',function(event){
+//     console.log('mouseleave');
+// })
+// rectangele.addEventListener('mousemove',function(event){
+//     console.log('mousemove');
+// })
