1647a092860350276be0b9b2c88305098fdda2ca
Dwisgolmog <rlaehdgus134@naver.com>
2023-07-05 16:46:34 +0900

    회원가입 추가 및 env 파일 추가
    회원가입을 위한 ejs 파일 및 server.js 파일 업데이트 및 환경변수를 사용하기 위해 npm install dotenv 를 설치하고 8번줄에 포함시켜 놓았음

--- 동현/nodejs/server.js
+++ 동현/nodejs/server.js
@@ -3,7 +3,9 @@ const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 const MongoClient = require('mongodb').MongoClient;
-
+//crypto를 사용해서 비밀번호 암호화하기
+const crypto = require('crypto')
+require('dotenv').config();
 
 //ejs 사용
 app.set('view engine', 'ejs');
@@ -17,12 +19,12 @@ app.use('/public', express.static('public'));
 
 var db;
 //몽고 DB 연결
-MongoClient.connect('mongodb+srv://donghyung2000:qwer1234@cluster0.tjqsheh.mongodb.net/', { useUnifiedTopology: true }, function (e, client) {
+MongoClient.connect(process.env.DB_URL, { useUnifiedTopology: true }, function (e, client) {
     if (e) return console.log(e);
 
     db = client.db('todoapp');
 
-    app.listen(8080, function () {
+    app.listen(process.env.PORT, function () {
         console.log('listening on 8080')
     })
 
@@ -121,6 +123,7 @@ app.put('/edit', function (req, res) {
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const session = require('express-session');
+const GridFSBucket = require('mongodb/lib/gridfs-stream');
 
 app.use(session({ secret: '비밀코드', resave: true, saveUninitialized: false }));
 app.use(passport.initialize());
@@ -130,18 +133,20 @@ app.use(passport.session());
 //How(어떻게 인증할지) 작성
 // 형식 --> LocalStrategy( { 설정 }, function(){ 아이디비번 검사하는 코드 } )
 passport.use(new LocalStrategy({
-    usernameField: 'id', //사용자가 제출한 아이디 위치
+    usernameField: 'email', //사용자가 제출한 아이디 위치
     passwordField: 'pw', //사용자가 제출한 비번 위치
     session: true,       //세션 유무
     passReqToCallback: false, //다른 정보 필요 유무
 }, function (입력한아이디, 입력한비번, done) {
-    //console.log(입력한아이디, 입력한비번);
-    db.collection('login').findOne({ id: 입력한아이디 }, function (에러, 결과) {
-        if (에러) return done(에러)
+    console.log(입력한아이디, 입력한비번);
+    db.collection('login').findOne({ email: 입력한아이디 }, function (에러, 결과) {
+        if (에러) return done(`login error!!:${에러}`);
 
         if (!결과) return done(null, false, { message: '존재하지않는 아이디요' })
-        //암호화 필요!!
-        if (입력한비번 == 결과.pw) {
+        //암호화한 값 불러오기
+        //console.log(결과.salt);
+        const pw = hashTest(결과.salt,입력한비번);
+        if (pw == 결과.pw) {
             //성공시 해달 결과값이 아래의 serializeUser의 user에 저장됨
             return done(null, 결과)
         } else {
@@ -161,7 +166,7 @@ app.post('/login', passport.authenticate('local', { failureRedirect: '/fail' }),
 })
 
 app.get('/mypage', loginCheck, function (req, res) {
-    res.render('myPage.ejs',{user: req.user});
+    res.render('myPage.ejs', { user: req.user });
 })
 
 //미들웨어 검사 함수
@@ -175,13 +180,54 @@ function loginCheck(req, res, next) {
 
 //유저의 id 데이터로 세션데이터 만들어줌 -->세션데이터아이디를 쿠키로 만들어 브라우저로 보내줌
 passport.serializeUser(function (user, done) {
-    done(null, user.id)
+    done(null, user.email)
 });
 
-//마이페이지 접속시 실행되는 함수
-//로그인한 사용자의 정보를 넘겨줌
+// 마이페이지 접속시 실행되는 함수
+// 로그인한 사용자의 정보를 넘겨줌
 passport.deserializeUser(function (아이디, done) {
-    db.collection('login').findOne({id:아이디},function(e,result){
+    db.collection('login').findOne({ email: 아이디 }, function (e, result) {
         done(null, result)
     })
 });
+
+app.get('/signup', function (req, res) {
+    res.render('signUp.ejs');
+})
+
+app.post('/signUp', function (req, res) {
+    //아이디 중복확인
+    db.collection('login').findOne({ email: req.body.email }, function (e, result) {
+        //중복이 아닐시
+        if (result != null) {
+            //아이디가 중복임을 알리고 회원가입 다시하기
+            res.send("<script>alert('아이디가 중복입니다! 다시 시도해주세요.'); window.location.href = '/signUp';</script>");
+        } else { //중복이 아닐시
+            //비밀번호 암호화
+            const pwInformation = hashTest(null,req.body.pw);            
+            //회원가입창에 있는 내용을 DB에 저장
+            db.collection('login').insertOne({ email: req.body.email,salt:pwInformation[0] ,pw: pwInformation[1], ad1: req.body.ad1, ad2: req.body.ad2, name: req.body.name }, function (e, result) {
+                if (e) console.log(`error!!:${e}`);
+            })
+
+            //회원가입 성공시 alert창과 함께 로그인 페이지로 이동
+            res.send("<script>alert('회원 가입 성공!'); window.location.href = '/login';</script>");  
+
+        }
+    })
+})
+
+//암호화 해주는 함수
+function hashTest(salt,password) {
+    //salt에 null값이 들어왔을때는 회원가입으로 간주하고 salt값 생성
+    if(salt == null){
+        const pwInformation = [];
+        //salt를 생성해서 db에 저장
+        const salt = crypto.randomBytes(32).toString('hex')
+        pwInformation.push(salt);
+        pwInformation.push(crypto.pbkdf2Sync(password, salt, 1, 32, 'sha512').toString('hex'));
+        return pwInformation;
+    }
+
+    return crypto.pbkdf2Sync(password, salt, 1, 32, 'sha512').toString('hex')
+}
