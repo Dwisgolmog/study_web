fff9e3be02d92558a72def75450f527ff978c50c
Dwisgolmog <rlaehdgus134@naver.com>
2023-07-24 18:23:14 +0900

    array, object state 변경법
    state 변경함수를 사용할때 먼저 기존 state의 값과 새로운 state의 값을 비교한후 다르면 변경해준다. 즉 변경점이 없으면 실행이 되지 않는다.
    
    object, array의 경우 변수를 저장할때 메모리의 주소값을 저장한다.(화살표) 때문에 일반적인 const copy = array 일 경우 해당 주소를 가져오는 것이기때문에 copy를 변경하면 array 값또한 변경이 된다.
    
    const copy = array;
    copy[0] = "change";
    setTitle(copy);
    
    해당 방식으로 할 경우 copy가 가르키는 주소안에 값은 변경이 되엇지만 copy 값은 array와 동일하기 때문에 setTitle은 작동이 되지 않는다.
    
    때문에 13번줄에 [...title]과 같이 주소값을 주는것이 아닌 안에있는 값을 넘겨 줌으로써 state 변경함수가 작동하도록 함과 더불어 원보 데이터의 값을 보존한다.

--- 동현/react/reactstudy/src/App.js
+++ 동현/react/reactstudy/src/App.js
@@ -1,6 +1,3 @@
-/* eslint-disable */
-// waring 메세지를 없애줌
-
 import './App.css';
 import {useState} from 'react';
 
@@ -8,15 +5,23 @@ function App() {
   //Destructuring
   //state의 사용 이유
   //일반 변수를 사용하는것과 달리 state는 내용의 변경이 있을시 html이 자동 재렌더링이 됌
-  let [title,setTitle] = useState(['man shirt','study react','mapleStory']);
+  const name = ['man shirt','study react','mapleStory']
+  let [title,setTitle] = useState(name);
   let [good,setGood] = useState([0,1,2]);
 
+  function modifyTitle(){
+      const copyTitle = [...title];
+      copyTitle[0] = 'Women shirt';
+      setTitle(copyTitle);
+  }
+
   return (
+    
     <div className="App">
       <div className='black-nav'>
-        <h4>Blog <span onClick={()=>{title[0] = 'Women shirt'; setTitle([...title])}}>ChangeWomen</span></h4>
+        <h4>Blog <span onClick={modifyTitle}>ChangeWomen</span></h4>
       </div>
-  
+
       {title.map((e, index) =>
         <div className='list' key={index}>
           <h4>{e} <span onClick={() => { setGood(good[index] + 1) }}>👍</span> {good[index]}</h4>
