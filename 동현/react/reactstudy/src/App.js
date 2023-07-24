f9b7b9180eab913571e8dbf3836048bd33733088
Dwisgolmog <rlaehdgus134@naver.com>
2023-07-24 15:47:36 +0900

    JSX 문법 사용법 및 useState
    JSX 문법
    1.class 넣을 땐 기존에 class =“” 가 아닌 className을 사용한다.
    2.html 태그 안에 변수를 넣고 싶을때는 {}를 사용한다 => {변수명}
    3.style을 직접 넣을때는 객체의 형태로 넣어야 한다.
    Ex) style={{이름:’값’ , 이름:’값’}}
    
    useState(‘보관할 자료’)
    이때 나오는 값은 [자료값,자료 변경 함수] 이다.
    ❗️ 자료값은 읽을때만 사용하며 수정해서는 안된다.
    ❗️ 자료변경 함수는 수정할때 호출한다.
    Ex) 자료변경 함수(자료값 + 10)
    
    때문에 const [name,setName] = useState(‘Kim’) 이와 같이 사용한다.
    
    해당 useState의 사용이유?
    state는 변동사항이 생기면 state를 쓰는 html을 자동으로 “재렌더링” 해주기 때문이다. 이는 안의 html 안에 state값을 변경하게 된다면 변경된 값으로 자동으로 다시 html을 보여주기 때문에 편리하다.

--- 동현/react/reactstudy/src/App.js
+++ 동현/react/reactstudy/src/App.js
@@ -9,26 +9,20 @@ function App() {
   //state의 사용 이유
   //일반 변수를 사용하는것과 달리 state는 내용의 변경이 있을시 html이 자동 재렌더링이 됌
   let [title,setTitle] = useState(['man shirt','study react','mapleStory']);
-  let [good,setGood] = useState(0);
+  let [good,setGood] = useState([0,1,2]);
 
   return (
     <div className="App">
       <div className='black-nav'>
         <h4>Blog <span onClick={()=>{title[0] = 'Women shirt'; setTitle([...title])}}>ChangeWomen</span></h4>
       </div>
-      <div className='list'>
-        <h4>{title[0]} <span onClick={()=> {setGood(good+1)}}>👍</span> {good}</h4>
-        <p>Date</p>
-      </div>
-      <div className='list'>
-        <h4>{title[1]} <span onClick={()=> {setGood(good+1)}}>👍</span> {good}</h4>
-        <p>Date</p>
-      </div>
-      <div className='list'>
-        <h4>{title[2]} <span onClick={()=> {setGood(good+1)}}>👍</span> {good}</h4>
-        <p>Date</p>
-      </div>
-      
+  
+      {title.map((e, index) =>
+        <div className='list' key={index}>
+          <h4>{e} <span onClick={() => { setGood(good[index] + 1) }}>👍</span> {good[index]}</h4>
+          <p>Date</p>
+        </div>
+      )}      
     </div>
   );
 }
