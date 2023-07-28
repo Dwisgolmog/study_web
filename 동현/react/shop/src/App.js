df1a4c6e4b3870f8279867f5d0c71cc23762bc9b
Dwisgolmog <rlaehdgus134@naver.com>
2023-07-28 18:02:11 +0900

    NavBar생성 및 폰트 설정
    React에서 폰트 설정하는법
    1.폰트를 다운 받은 후 public같은 파일에 넣어준다
    2.App.css와 같은 파일에 font-face를 추가해준다.
    3.이후 클래스 설정 등으로 사용하기
    4.MUI경우 createTheme을 통하여     typography:{
            fontFamily: "'Handjet', cursive"
        }, 폰트를 설정해주고
    ThemeProvider로 태그를 감싸고 theme={darkTheme} 와 같이 적용한다
    
    MUI로 NavBar 만들기
    AppBar를 import 하여 기본적인 틀을 생성한다.
    이후 ToolBar를 통하여 요소를 묶어서 작성한다.
    Tabs의 경우 아래의 블로그 참고
    https://hyogeun.tistory.com/165

--- 동현/react/shop/src/App.js
+++ 동현/react/shop/src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
-import Header from './components/Head';
-import Article from './components/Article';
 import { Container } from '@mui/system';
-import { Grid } from '@mui/material';
 
 function App() {
   return (
+    <>
+    <NavBar></NavBar>
     <Container fixed className="App">
-      <Header></Header>
-      <Grid container>
-        <Grid item xs="2">
-          <NavBar></NavBar>
-        </Grid>  
-        <Grid item xs="10">
-          <Article></Article>
-        </Grid>   
-      </Grid>
+      
+
     </Container>
+    </>
   );
 }
 
--- 동현/react/shop/src/components/NavBar.js
+++ 동현/react/shop/src/components/NavBar.js
@@ -1,13 +1,66 @@
-import React from "react";
+import {React,useState} from "react";
+import { AppBar,Box, Toolbar,Typography,Tabs,Tab } from "@mui/material";
+import AdbIcon from '@mui/icons-material/Adb';
+import {ThemeProvider,createTheme} from "@mui/material";
+import '../App.css';
+
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+        primary: {
+            main: '#212121', // black 계열의 색상으로 설정
+        },
+    },
+    typography:{
+        fontFamily: "'Handjet', cursive"
+    },
+
+});
 
 function NavBar() {
+    const [value, setValue] = useState(0);
+
+    const handleChange = (event, newValue) => {
+      setValue(newValue);
+    };
+
     return (
-        <nav>
-            <ol>
-                <li>html</li>
-                <li>css</li>
-            </ol>
-        </nav>
+        <ThemeProvider theme={darkTheme}>
+            <Box sx={{flexGow: 1,}}>
+                <AppBar enableColorOnDark position="static">
+                    <Toolbar>
+                        <AdbIcon sx={{ m: 2, fontSize:50 }} />
+                        <Typography
+                            className="Handjet"
+                            variant="h3"
+                            noWrap
+                            component="a"
+                            href="/"
+                            sx={{
+                                fontWeight: 700,
+                                letterSpacing: '.3rem',
+                                color: 'inherit',
+                                textDecoration: 'none',
+                            }}>
+                            SHOP
+                        </Typography>
+
+                        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                            <Tabs indicatorColor="secondary" sx={{
+                                ml: 1, '& .MuiTab-root': {
+                                    fontSize: '1.2rem',
+                                },
+                            }} value={value} onChange={handleChange} textColor="inherit">
+                                <Tab label="Item One" />
+                                <Tab label="Item Two" />
+                                <Tab label="Item Three" />
+                            </Tabs>
+                        </Box>
+
+                    </Toolbar>
+                </AppBar>
+            </Box>
+        </ThemeProvider>    
     );
 }
 
