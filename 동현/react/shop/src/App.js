52d68095657b35d9e8b3727aedf8046ef0dafdc5
Dwisgolmog <rlaehdgus134@naver.com>
2023-07-30 21:02:15 +0900

    이미지 삽입
    html 안에서 이미지 사용시 이미지를 먼저 import 해야 사용할 수 있다.
    
    Grid xs는 일반적인 화면크기에서의 아이템 크기를 조절한다.
    sm의 경우 600px
    md는 900px 이다.
    
    public 폴더안에 이미지 저장시
    따로 import 없이 그냥 /이미지 경로를 입력하면 된다.
    ex) src='/image.png' 와 같이 사용
    단  서브경로를 사용할시(index/shop/product 와 같은 사이트) 문제가 발생 할 수 있으므로 아래와 같은 방식으로 경로를 사용해야함
    src={process.env.PUBLIC_URL
     +'/image.png' }

--- 동현/react/shop/src/App.js
+++ 동현/react/shop/src/App.js
@@ -1,15 +1,37 @@
 import React from 'react';
-import './App.css';
 import NavBar from './components/NavBar';
 import { Container } from '@mui/system';
+import bg from './img/bg.png';
+import { Grid } from '@mui/material';
 
 function App() {
   return (
     <>
     <NavBar></NavBar>
     <Container fixed className="App">
-      
+      <div className='main-bg' style={{backgroundImage : `url(${bg})`}}></div>
 
+      <Container >
+        <Grid container className='item'>
+          <Grid item xs={4} sm={4} md={4}>
+            <img src='https://codingapple1.github.io/shop/shoes1.jpg' />
+            <div>title</div>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+          </Grid>
+          
+          <Grid item xs={4} sm={4} md={4}>
+            <img src='https://codingapple1.github.io/shop/shoes2.jpg' />
+            <div>title</div>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+          </Grid>
+          
+          <Grid item xs={4} sm={4} md={4}>  
+            <img src='https://codingapple1.github.io/shop/shoes3.jpg' />
+            <div>title</div>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+          </Grid>
+        </Grid>
+      </Container> 
     </Container>
     </>
   );
