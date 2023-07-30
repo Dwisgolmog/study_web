752de3135bf4160ea8b8ea596dfa29c8d30d56c4
Dwisgolmog <rlaehdgus134@naver.com>
2023-07-30 21:23:47 +0900

    import export 문법
    다른 파일에 있는 데이터를 가져오고 싶을때 작성한 파일의 맨 아랫줄에
    export default 변수명
    으로 작성하고 불러올때는
    import 변수명 from 파일경로
    를 통해 사용이 가능하다. 만약에 여러 변수를 쓰고 싶을시 export 문을 아래와 같이 변경해주면 된다.
    exrpot {a,b} 이라 작성하고 불러올때도 똑같이 작성해준다.
    import {a,b} from 파일경로

--- 동현/react/shop/src/App.js
+++ 동현/react/shop/src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import {React,useState} from 'react';
 import NavBar from './components/NavBar';
 import { Container } from '@mui/system';
 import bg from './img/bg.png';
 import { Grid } from '@mui/material';
+import data from './data.js';
 
 function App() {
+  const [shoes,setShoes] = useState(data);
+
   return (
     <>
     <NavBar></NavBar>
@@ -15,20 +18,23 @@ function App() {
         <Grid container className='item'>
           <Grid item xs={4} sm={4} md={4}>
             <img src='https://codingapple1.github.io/shop/shoes1.jpg' />
-            <div>title</div>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+            <div>{shoes[0].title}</div>
+            <p>{shoes[0].content}</p>
+            <p>{shoes[0].price}</p>
           </Grid>
           
-          <Grid item xs={4} sm={4} md={4}>
+          <Grid item xs={4} sm={4} md={4}> 
             <img src='https://codingapple1.github.io/shop/shoes2.jpg' />
-            <div>title</div>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+            <div>{shoes[1].title}</div>
+            <p>{shoes[1].content}</p>
+            <p>{shoes[1].price}</p>
           </Grid>
           
           <Grid item xs={4} sm={4} md={4}>  
             <img src='https://codingapple1.github.io/shop/shoes3.jpg' />
-            <div>title</div>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+            <div>{shoes[2].title}</div>
+            <p>{shoes[2].content}</p>
+            <p>{shoes[2].price}</p>
           </Grid>
         </Grid>
       </Container> 
---
+++ 동현/react/shop/src/data.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+      id : 0,
+      title : "White and Black",
+      content : "Born in France",
+      price : 120000
+    },
+  
+    {
+      id : 1,
+      title : "Red Knit",
+      content : "Born in Seoul",
+      price : 110000
+    },
+  
+    {
+      id : 2,
+      title : "Grey Yordan",
+      content : "Born in the States",
+      price : 130000
+    }
+]
+
+export default data;
