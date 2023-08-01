2e016afe96e8a0c7da042598d4ddcc166a4e6706
Dwisgolmog <rlaehdgus134@naver.com>
2023-08-01 15:19:18 +0900

    상품 컴포넌트화

--- 동현/react/shop/src/App.js
+++ 동현/react/shop/src/App.js
@@ -1,12 +1,13 @@
 import {React,useState} from 'react';
 import NavBar from './components/NavBar';
+import ProductCard from './components/product';
 import { Container } from '@mui/system';
 import bg from './img/bg.png';
 import { Grid } from '@mui/material';
 import data from './data.js';
 
 function App() {
-  const [shoes,setShoes] = useState(data);
+  const [shoes] = useState(data);
 
   return (
     <>
@@ -16,26 +17,10 @@ function App() {
 
       <Container >
         <Grid container className='item'>
-          <Grid item xs={4} sm={4} md={4}>
-            <img src='https://codingapple1.github.io/shop/shoes1.jpg' />
-            <div>{shoes[0].title}</div>
-            <p>{shoes[0].content}</p>
-            <p>{shoes[0].price}</p>
-          </Grid>
           
-          <Grid item xs={4} sm={4} md={4}> 
-            <img src='https://codingapple1.github.io/shop/shoes2.jpg' />
-            <div>{shoes[1].title}</div>
-            <p>{shoes[1].content}</p>
-            <p>{shoes[1].price}</p>
-          </Grid>
-          
-          <Grid item xs={4} sm={4} md={4}>  
-            <img src='https://codingapple1.github.io/shop/shoes3.jpg' />
-            <div>{shoes[2].title}</div>
-            <p>{shoes[2].content}</p>
-            <p>{shoes[2].price}</p>
-          </Grid>
+          {shoes.map((product,index) => {
+            return <ProductCard key={index} product = {product} index = {index+1}></ProductCard>
+          })}
         </Grid>
       </Container> 
     </Container>
---
+++ 동현/react/shop/src/components/product.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Grid } from '@mui/material';
+import '../App.css';
+
+function ProductCard(props){
+    return(
+        <>
+            <Grid item xs={4} sm={4} md={4}>
+                <img src={`https://codingapple1.github.io/shop/shoes${props.index}.jpg`} />
+                <div>{props.product.title}</div>
+                <p>{props.product.content}</p>
+                <p>{props.product.price}</p>
+          </Grid>
+          
+        </>
+    );
+}
+
+export default ProductCard
