e8ea4741b71c851a439e0dfe72664344d2fe16d3
Dwisgolmog <rlaehdgus134@naver.com>
2023-08-02 15:47:58 +0900

    Route: url 파라미터 사용하기
    경로의 /뒷부분에 :(클론)변수명 을 이용하여 /이후의 입력한 값을 페이지에 넘길수 있다.
    해당 값을 받기 위해서는 useParams라는 훅을 import 한후 useParams()를 변수에 저장하여 사용하면 된다.

--- 동현/react/shop/src/App.js
+++ 동현/react/shop/src/App.js
@@ -1,19 +1,22 @@
-import {React} from 'react';
+import {React,useState} from 'react';
 import NavBar from './components/NavBar';
 import { Container } from '@mui/system';
-import {Routes, Route} from 'react-router-dom';
+import {Routes, Route, UNSAFE_DataRouterStateContext} from 'react-router-dom';
 import HomePage from './components/HomePage';
 import Detail from './components/detail';
 import Event from './Page/Event';
+import data from './data';
 
 function App() {
+  const [shoes] = useState(data);
+
   return (
     <>
     <NavBar></NavBar>
     <Container fixed className="App">
       <Routes>
         <Route path='/' element={<HomePage></HomePage>}/>
-        <Route path='/detail' element={<Detail></Detail>}/>
+        <Route path='/detail/:id' element={<Detail shoes={shoes}></Detail>}/>
         <Route path='/event' element={<Event></Event>}>
           <Route path='one' element={<div>첫 주문시 양배추즙 서비스</div>} />
           <Route path='two' element={<div>생일기념 쿠폰 받기</div>}/>
--- 동현/react/shop/src/components/detail.js
+++ 동현/react/shop/src/components/detail.js
@@ -2,17 +2,21 @@ import React from 'react'
 import { Container } from '@mui/system';
 import { Grid,Button } from '@mui/material';
 import '../App.css'
+import { useParams } from 'react-router-dom';
 
-function Detail() {
+function Detail(props) {
+    let {id} = useParams();
+    let imageId = parseInt(id)+1;
+    
     return (
         <div className='detail'>
-            <img src="https://codingapple1.github.io/shop/shoes1.jpg" style={{ width: '70%' }} />
+            <img src={`https://codingapple1.github.io/shop/shoes${imageId}.jpg`} style={{ width: '70%' }}/>
             <Container>
                 <Grid container className='item'>
                     <Grid item xs={12}>
-                        <h4 className="pt-5">상품명</h4>
-                        <p>상품설명</p>
-                        <p>120000원</p>
+                        <h4 className="pt-5">{props.shoes[id].title}</h4>
+                        <p>{props.shoes[id].content}</p>
+                        <p>{props.shoes[id].price}</p>
                         <Button variant="outlined">주문하기</Button>
                     </Grid>
                 </Grid>
